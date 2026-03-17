77573813de6da48bc59247b22e8bbfc097324fd7
paulsurya <paulsurya11@gmail.com>
2026-03-18 05:17:33 +0530

    Changed the dependency on secrets.toml

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import google.generativeai as genai
 from youtube_transcript_api import YouTubeTranscriptApi
 
-genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key="AIzaSyAmB9n_2nRtfLI4VtBO-r584IuzZUfjj5o")
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
 def extract_video_id(url):
